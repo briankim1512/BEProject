fe8ad49a5c4a5d745a8dd0a368a0afc57464f3e1
Brian Kim <briankim1512@gmail.com>
2017-07-27 06:02:04 +0900

    Added the disconnect functionality and added more comments to clarify process

--- server.py
+++ server.py
@@ -209,6 +209,42 @@ def gconnect():
     login_session['picture'] = data["picture"]
     login_session['email'] = data["email"]
 
+    return 'Welcome'+login_session['username']
+
+
+# Route for disconnecting a user from the webapp
+@app.route('/gdisconnect')
+def gdisconnect():
+    # Make sure the web disconnects the connected user, not everyone
+    credentials = login_session.get('credentials')
+    if credentials is None:
+        response = make_response(json.dumps('Current user not connected'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    # Revoke the current token from session
+    access_token = credentials
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+
+    # If google returns the A-OK, delete the session cache
+    if result['status'] == '200':
+        del login_session['credentials']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully Disconnected'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    # If something else happened, send the an error message to the user
+    else:
+        response = make_response(json.dumps('Something went wrong...'), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
 if __name__ == '__main__':
     app.secret_key = "OIHFG4HOP398HFLKJ43HT2498"
     app.debug = True
